af981337c8490670d95d5be99d2d16de4d31266a
Nihaljain33 <nihaljain33@gmail.com>
2024-01-28 20:52:50 +0530

    Agnostic Binary Search

--- src/algorithms/BinarySearch.java
+++ src/algorithms/BinarySearch.java
@@ -7,21 +7,37 @@ public class BinarySearch {
         static int binarySearch(int[] arr, int target) {
             int start = 0;
             int end = arr.length - 1;
+            if(arr.length != 0 && arr[start] < arr[end]) {
+                while(start <= end) {
+                    // find the middle element
+                    // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+                    int mid = start + (end - start) / 2;
 
-            while(start <= end) {
-                // find the middle element
-                // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-                int mid = start + (end - start) / 2;
-
-                if (target < arr[mid]) {
-                    end = mid - 1;
-                } else if (target > arr[mid]) {
-                    start = mid + 1;
-                } else {
-                    // Index found
-                    return mid;
+                    if (target < arr[mid]) {
+                        end = mid - 1;
+                    } else if (target > arr[mid]) {
+                        start = mid + 1;
+                    } else {
+                        // Index found
+                        return mid;
+                    }
                 }
+                return -1;
+            } else {
+                while (start <= end) {
+                    // find the middle element
+                    int mid = start + (end - start) / 2;
+
+                    if (target < arr[mid]) {
+                        start = mid - 1;
+                    } else if (target > arr[mid]) {
+                        end = mid + 1;
+                    } else {
+                        // Index found
+                        return mid;
+                    }
+                }
+                return -1;
             }
-            return -1;
         }
 }
